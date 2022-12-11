fb6ef03734084e6b0fcba8712286696602692c7c
Faridhosen4563 <faridhosen790@gmail.com>
2022-12-12 01:48:14 +0600

    Add logo in Navbar

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -1,8 +1,10 @@
+import Image from 'next/image';
 import React from 'react';
+import logo from '../public/next_logo.png'
 
 const NavBar = () => {
     return (
-        <div className="navbar  container flex mx-auto">
+        <div className="navbar  container flex mx-auto md:h-24">
             <div className="navbar-start">
                 <div className="dropdown">
                     <label tabIndex={0} className="btn btn-ghost lg:hidden">
@@ -24,7 +26,7 @@ const NavBar = () => {
                         <li><a>Build Resume</a></li>
                     </ul>
                 </div>
-                <a className="btn btn-ghost normal-case text-xl">Next-Jobs</a>
+                <a className="btn btn-ghost normal-case text-xl"><Image src={logo} className='w-24 h-18 hidden md:block'></Image> Next-Job</a>
             </div>
             <div className="navbar-center hidden lg:flex">
                 <ul className="menu menu-horizontal px-1">
@@ -46,7 +48,7 @@ const NavBar = () => {
             <div className="navbar-end">
                 <button><a className=" btn-xs mx-3">Log In</a></button>
                 <button><a className=" btn-xs mx-3">Register</a></button>
-                
+
             </div>
         </div>
     );
