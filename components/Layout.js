3011911a127564fd6ec5fce8207bd978b2ad2909
khalidhasansuhas <khalidhasansuhas@gmail.com>
2022-12-11 23:14:10 +0600

    layout created

--- components/Layout.js
+++ components/Layout.js
@@ -6,19 +6,21 @@ import Content from "./Content";
 export default function Layout({ children }) {
     return (
         <>
-            <NavBar></NavBar>
-            <main className="grid sm:grid-flow-row-dense sm:grid-cols-6 sm:grid-rows-6 ">
+            <div className="sm:h-full">
+                <NavBar></NavBar>
+                <main className="grid sm:grid-flow-row-dense sm:grid-cols-6 sm:grid-rows-6 sm:h-screen">
 
-                <div className="sm:col-span-1">
-                    <LeftSideNav></LeftSideNav>
-                </div>
-                <div className="sm:col-span-5 gap-2 my-5">
-                    <Content></Content>
+                    <div className="sm:col-span-1  shadow-lg">
+                        <LeftSideNav></LeftSideNav>
+                    </div>
+                    <div className="sm:col-span-5 gap-2 my-5">
+                        <Content></Content>
 
-                </div>
+                    </div>
 
-            </main>
-            <Footer></Footer>
+                </main>
+                <Footer></Footer>
+            </div>
         </>
     )
 }
--- components/LeftSideNav.jsx
+++ components/LeftSideNav.jsx
@@ -2,15 +2,44 @@ import React from 'react';
 
 const LeftSideNav = () => {
     return (
-        <div className='container h-full w-full'>
-            <div className="drawer h-full">
-               
+        <div className='container w-full'>
+            <div className="drawer">
+
                 <div className="drawer-side">
                     <label htmlFor="my-drawer-2" className="drawer-overlay"></label>
                     <ul className="menu h-full p-4 w-full  text-base-content">
 
-                        <li><a>Sidebar Item 1</a></li>
-                        <li><a>Sidebar Item 2</a></li>
+                        <div className='w-full p-3'>
+                        <label htmlFor="Select Job Category"> Select Job Type</label>
+                            <select name='categoryName' className="w-full px-4 py-3 rounded-md" required>
+                                <option selected disabled>Job Type</option>
+                                <option >On Site</option>
+                                <option>Remote</option>
+                                <option>Hybrid</option>
+                            </select>
+                        </div>
+                        <div className='w-full p-3'>
+                        <label htmlFor="Select Job Category"> Select Job Type</label>
+                            <select name='categoryName' className="w-full px-4 py-3 rounded-md" required>
+                                <option selected disabled>Job Category</option>
+                                <option >Internship</option>
+                                <option>Full Time</option>
+                                <option>Part Time</option>
+                            </select>
+                        </div>
+                        <div className='w-full p-3'>
+                            <h3>Minimum Salary Expectation</h3>
+                        <fieldset className="space-y-1 sm:w-48 dark:text-gray-100">
+                            <input type="range" className="w-full accent-violet-400" min="1" max="5" />
+                            <div aria-hidden="true" className="flex justify-between px-1">
+                                <span>0</span>
+                                <span>2k</span>
+                                <span>5k</span>
+                                <span>10k</span>
+                                <span>20k</span>
+                            </div>
+                        </fieldset>
+                        </div>
                     </ul>
 
                 </div>
